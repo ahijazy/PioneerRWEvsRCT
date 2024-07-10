b60f755b5c0f0777437433fd34cac636f0dbb783
ahijazy <94984907+ahijazy@users.noreply.github.com>
2024-07-10 12:21:23 +0200

    Add files via upload

---
+++ inst/settings/insert cohort definitions.r
@@ -0,0 +1,16 @@
+library(ROhdsiWebApi)
+
+baseUrl <-"https://pioneer.hzdr.de/WebAPI"
+
+token <- 'Bearer '
+setAuthHeader(baseUrl = baseUrl, token)
+
+# after inserting the cohorts
+ 
+# Insert cohort definitions from ATLAS into package -----------------------
+ROhdsiWebApi::insertCohortDefinitionSetInPackage(fileName = "inst/settings/CohortsToCreate.csv",
+baseUrl = baseUrl ,
+insertTableSql = TRUE,
+insertCohortCreationR = TRUE,
+generateStats = FALSE,
+packageName = "PioneerRWEvsRCT")
